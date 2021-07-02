714bbd424e6e903fcdcf2cafc5c3346f48805c80
hounaikuo <hounaikuo@canaan-creative.com>
2021-07-02 11:07:18 +0800

    Update SparkApplication.java

--- src/main/java/com/hnk/spark/SparkApplication.java
+++ src/main/java/com/hnk/spark/SparkApplication.java
@@ -20,6 +20,50 @@ public class SparkApplication {
         SparkConf sparkConf = new SparkConf().setAppName("sparkBoot").setMaster("local");
 
         JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+        run(sparkContext);
+    }
+
+    public static void run(JavaSparkContext sparkContext) {
+        JavaRDD<String> rdd = sparkContext.parallelize(Arrays.asList("t est", "t ava", "p ython"));
+
+        //把RDD的第一个字符当做Key
+        PairFunction<String, String, String> pairFunction = new PairFunction<String, String, String>() {
+            @Override
+            public Tuple2<String, String> call(String s) throws Exception {
+                return new Tuple2<>(s.split(" ")[0], s);
+            }
+        };
+
+
+        //此处创建好pairRDD
+        JavaPairRDD<String, String> pairRdd = rdd.mapToPair(pairFunction);
+
+        //下层都是对pairRDD的操作演示
+
+        /*合并含有相同键的值*/
+        JavaPairRDD<String, String> rdd1 = pairRdd.reduceByKey((v1, v2) -> v1 + v2).groupByKey().mapValues(v1 -> v1 + "sirZ");
+
+        /*相同key的元素进行分组*/
+//        pairRdd.groupByKey();
+//
+//        /*对pair中的每个值进行应用*/
+//        pairRdd.mapValues( v1 -> v1 + "sirZ");
+
+        /*返回只包含键的RDD*/
+        pairRdd.keys();
+
+        /*返回只包含值的RDD*/
+        pairRdd.values();
+
+        /*返回根据键排序的RDD*/
+        pairRdd.sortByKey();
+
+        rdd1.foreach(elem -> {
+            System.out.println(elem._1 + "-----" + elem._2);
+        });
+    }
+
+    public static void wordCount(JavaSparkContext sparkContext){
 
         JavaRDD<String> textFile = sparkContext.textFile("/Users/hounaikuo/Documents/project/map-reduce/data/word");
         JavaPairRDD<String, Integer> counts = textFile
